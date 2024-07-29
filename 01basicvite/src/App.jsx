7823a3cee0ee9b5f040b3db930761912916860c7
Shreelaxmi123 <shreelaxminakshathri@gmail.com>
2024-07-29 19:24:40 +0530

    vite changes

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,11 +1,11 @@
 import Youtube from './Youtube';
 
 function App() {
-
+  const userName = 'Shreelaxmi';
   return (
     <>
-      <h1>Vite react app</h1>
-      <h1>Vite react app</h1>
+      <h1>Vite react app {2+2}</h1>
+      <h1>Vite react app {userName} </h1>
       <Youtube/>
     </>
   )
